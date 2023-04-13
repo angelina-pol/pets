a892d2def86ab3e082fd579e6108c942c84f7f87
Angelina <gela-turlova@inbox.ru>
2023-04-13 18:09:10 +0300

    added onclick for math operation

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -5,6 +5,10 @@ window.onload = init;
 function init() {
     let del = document.getElementById('delete');
     let num = document.getElementsByClassName('number');
+    let plus = document.getElementById('plus');
+    let minus = document.getElementById('minus');
+    let mul = document.getElementById('mul');
+    let divide = document.getElementById('divide');
     let inpCurrent = document.getElementById('userInput');
     let inpResult;
 
@@ -12,11 +16,36 @@ function init() {
         num[i].onclick = function() {
             inpCurrent.value += num[i].innerHTML;
             inpResult = inpCurrent.value;
-            console.log(inpResult)
+            return inpResult;
         }
     }
 
     del.onclick = function() {
         inpCurrent.value = "";
     }
+
+    plus.onclick = function() {
+        inpCurrent.value = inpResult + plus.innerHTML;
+        inpResult = inpCurrent.value;
+        return inpResult;
+    }
+
+    minus.onclick = function() {
+        inpCurrent.value = inpResult + minus.innerHTML;
+        inpResult = inpCurrent.value;
+        return inpResult;
+    }
+
+    mul.onclick = function() {
+        inpCurrent.value = inpResult + mul.innerHTML;
+        inpResult = inpCurrent.value;
+        return inpResult;
+    }
+
+    divide.onclick = function() {
+        inpCurrent.value = inpResult + divide.innerHTML;
+        inpResult = inpCurrent.value;
+        return inpResult;
+    }
+
 }
